730f5a821ebb58e1c3e015d115cd7ed64282137f
Sylvain Gadrat <sgadrat@wontfix.it>
2014-07-31 13:58:19 +0000

    add cases

--- js/influance.js
+++ js/influance.js
@@ -1,14 +1,40 @@
+function Building(x, y) {
+	rtge.DynObject.call(this);
+	this.x = x;
+	this.y = y;
+
+	this.actions = [
+		'goto',
+	];
+}
+
+function Case(x, y) {
+	Building.call(this, x, y);
+	this.animation = 'building.case';
+}
+
 function init() {
+	var animations = {};
+
+	var animBuildingCase = new rtge.Animation();
+	animBuildingCase.steps = ['imgs/case.jpg'];
+	animBuildingCase.durations = [600000];
+	animations['building.case'] = animBuildingCase;
+
 	rtge.init(
 		'view',
 		{
 			'terrain': 'imgs/map.jpg',
-			'objects': []
+			'objects': [
+				new Case(2*16, 15*16),
+				new Case(17*16, 39*16),
+			]
 		},
-		{},
+		animations,
 		[],
 		[
-			'imgs/map.jpg'
+			'imgs/map.jpg',
+			'imgs/case.jpg',
 		],
 		{}
 	);
