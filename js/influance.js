126b14c7511c90513e1f093dec832d5ca0d14f56
Sylvain Gadrat <sgadrat@wontfix.it>
2014-07-31 16:15:55 +0000

    add some gui shown xhen selecting a building

--- js/influance.js
+++ js/influance.js
@@ -1,3 +1,7 @@
+var influance = {
+	selected: null,
+};
+
 function Building(x, y) {
 	rtge.DynObject.call(this);
 	this.x = x;
@@ -11,6 +15,12 @@ function Building(x, y) {
 function Case(x, y) {
 	Building.call(this, x, y);
 	this.animation = 'building.case';
+
+	this.portrait = 'imgs/case.jpg';
+
+	this.click = function(x, y) {
+		select(this);
+	}
 }
 
 function init() {
@@ -35,7 +45,45 @@ function init() {
 		[
 			'imgs/map.jpg',
 			'imgs/case.jpg',
+			'imgs/icons/action/goto.png',
 		],
-		{}
+		{
+			'worldClick': unselect
+		}
 	);
+}
+
+function select(o) {
+	actions = [];
+	for (var i = 0; i < o.actions.length; ++i) {
+		actions.push({
+			'icon': 'imgs/icons/action/'+ o.actions[i] +'.png',
+			'action': o.actions[i]
+		});
+	}
+	guiShowSelection(o.portrait, actions);
+
+	influance.selected = o;
+}
+
+function unselect() {
+	influance.selected = null;
+	guiHideSelection();
+}
+
+function action_goto() {
+}
+
+function guiShowSelection(portrait, actions) {
+	document.getElementById('selectportrait').src = portrait;
+	var actionList = '';
+	for (var i = 0; i < actions.length; ++i) {
+		actionList += '<li><img src="'+ actions[i]['icon'] +'" onclick="action_'+ actions[i]['action'] +'()" /></li>';
+	}
+	document.getElementById('selectactions').innerHTML = actionList;
+	document.getElementById('selection').style.visibility = 'visible';
+}
+
+function guiHideSelection() {
+	document.getElementById('selection').style.visibility = 'hidden';
 }
