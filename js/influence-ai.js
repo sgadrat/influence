e6734caa5ec88e3fa5836b55b40c5613c6481f8a
Sylvain Gadrat <sgadrat@wontfix.it>
2015-12-07 16:44:05 +0100

    Change behaviour tree definition
    Use a more concise description of the BT in a text format, than parse it. This avoid
    javascript syntax to interfer with lisibility. Also, functions callable from the behaviour
    tree have been put in "aiBehaviourTreeFunctions" instead of being globals.

--- js/influence-ai.js
+++ js/influence-ai.js
@@ -1,137 +1,278 @@
-/*eslint-disable indent*/
-var AI_CHARACTER_BEHAVIOUR = {
-	type: 'selector',
-	childs: [{
-		// Go to the pub
-		type: 'sequence',
-		childs: [{
-			// Check if the character needs to eat
-			type: 'leaf',
-			action: aiIsHungry
-		},
-		{
-			// Select an inn that can serve a meal
-			type: 'selector',
-			childs: [{
-				// Select an inn that already can serve a meal
-				type: 'leaf',
-				action: aiSelectInnWithMeal
-			},
-			{
-				// Put a meal in an inn, then select it
-				type: 'sequence',
-				childs: [{
-					// Get someting edible in inventory
-					type: 'selector',
-					childs: [{
-						// Already have it in inventory ?
-						type: 'leaf',
-						action: aiHaveEdibleInInventory
-					},
-					{
-						// Take it from one of our building
-						type: 'sequence',
-						childs: [{
-							type: 'leaf',
-							action: aiSelectDynastyBuildingStockingEdible
-						},
-						{
-							type: 'leaf',
-							action: aiGoToBuilding
-						},
-						{
-							type: 'leaf',
-							action: aiTakeEdibleFromStock
-						}]
-					},
-					{
-						// Buy it from the market
-						type: 'sequence',
-						childs: [{
-							type: 'leaf',
-							action: aiSelectClearingHouseHavingEdible
-						},
-						{
-							type: 'leaf',
-							action: aiGoToBuilding
-						},
-						{
-							type: 'leaf',
-							action: aiBuyOneEdible
-						}]
-					}]
-				},
-				{
-					// Select an inn to put our meal in
-					type: 'selector',
-					childs: [{
-						type: 'leaf',
-						action: aiSelectDynastyInn
-					},
-					{
-						// Build an inn
-						type: 'sequence',
-						childs: [{
-							type: 'leaf',
-							action: aiSelectBuyableVacantLot
-						},
-						{
-							type: 'leaf',
-							action: aiGoToBuilding
-						},
-						{
-							type: 'leaf',
-							action: aiBuyVacantLot
-						},
-						{
-							type: 'leaf',
-							action: aiBuildInn
-						},
-						{
-							type: 'leaf',
-							action: aiSelectDynastyInn
-						}]
-					}]
-				},
-				{
-					// Go to our inn
-					type: 'leaf',
-					action: aiGoToBuilding
-				},
-				{
-					// Put our meal in our inn
-					type: 'leaf',
-					action: aiPutEdibleToStock
-				}]
-			}]
-		},
-		{
-			// Go to the inn
-			type: 'leaf',
-			action: aiGoToBuilding
-		},
-		{
-			// Take a meal
-			type: 'leaf',
-			action: aiTakeMeal
-		}]
+var AI_CHARACTER_BEHAVIOUR_GRAPH = `
+?
+	->                            (Go to the pub)
+		IsHungry
+		?                         (Find an Inn that can serve a meal)
+			SelectInnWithMeal     (Simply select one that already can serve a meal)
+			->                    (Put a meal in an inn then select it)
+				?                 (Find some meal)
+					HaveEdibleInInventory
+					->            (Take it from one of our building)
+						SelectDynastyBuildingWithEdible
+						GoTo
+						TakeEdibleFromStock
+					->            (Buy it from the market)
+						SelectMarketWithEdible
+						GoTo
+						BuyEdible
+				?                 (Select an inn to put our meal in)
+					SelectDynastyInn
+					->            (Build an inn to select it)
+						SelectBuyableLot
+						GoTo
+						BuyLot
+						BuildInn
+						SelectDynastyInn
+				GoTo
+				PutEdibleToStock
+		GoTo
+		TakeMeal
+	->
+		SelectRandomBuilding
+		GoTo
+`;
+
+var aiBehaviourTreeFunctions = {
+	BuildInn: function (context) {
+		return aiDoAction(context, {
+			action: 'construct',
+			buildingName: 'inn',
+			actor: influence.characters[context['character']],
+			originalLot: context['selectedBuilding'],
+			callback: aiDefaultActionCallback
+		});
 	},
-	{
-		// Walk to a random building
-		type: 'sequence',
-		childs: [{
-			// Select a random building
-			type: 'leaf',
-			action: aiSelectRandomBuilding
-		},
+
+	BuyEdible: function (context) {
+		var edibles = ['pie', 'jam'];
+		for (var i = 0; i < edibles.length; ++i) {
+			if (context['selectedBuilding'].buy(context['character'], 'pie', 1)) {
+				return behaviourtree.SUCCESS;
+			}
+		}
+		return behaviourtree.FAIL;
+	},
+
+	BuyLot: function (context) {
+		return aiDoAction(context, {
+			action: 'buy',
+			actor: influence.characters[context['character']],
+			target: context['selectedBuilding'],
+			callback: aiDefaultActionCallback
+		});
+	},
+
+	GoTo: function (context) {
+		var character = influence.characters[context['character']];
+		if (character.currentAction == 'move') {
+			return behaviourtree.RUNNING;
+		}
+		if (context['selectedBuilding'] === null) {
+			return behaviourtree.FAIL;
+		}
+		var building = context['selectedBuilding'];
+		if (character.x == building.x && character.y == building.y) {
+			return behaviourtree.SUCCESS;
+		}
+		moveCharacter(character, {x:building.x, y:building.y}, building.indoor);
+		return behaviourtree.RUNNING;
+	},
+
+	HaveEdibleInInventory: function (context) {
+		var character = influence.characters[context['character']];
+		if (
+			character.inventory.containItems('pie', 1) ||
+			character.inventory.containItems('jam', 1)
+		)
 		{
-			// Go to the building
-			type: 'leaf',
-			action: aiGoToBuilding
-		}]
-	}]
+			return behaviourtree.SUCCESS;
+		}
+		return behaviourtree.FAIL;
+	},
+
+	IsHungry: function (context) {
+		var hoursSinceLastMeal = (getGameDate().getTime() - context['lastEat'].getTime()) / (60*60*1000);
+		if (hoursSinceLastMeal >= 24) {
+			return behaviourtree.SUCCESS;
+		}
+		return behaviourtree.FAIL;
+	},
+
+	PutEdibleToStock: function (context) {
+		var character = influence.characters[context['character']];
+		var building = context['selectedBuilding'];
+
+		if (typeof building.stock == 'undefined') {
+			return behaviourtree.FAIL;
+		}
+
+		var edibles = ['pie', 'jam'];
+		for (var i = 0; i < edibles.length; ++i) {
+			var item = edibles[i];
+			if (building.stock.hasSlotForItem(item) && character.inventory.containItems(item, 1)) {
+				character.inventory.removeItems(item, 1);
+				building.stock.addItems(item, 1);
+				return behaviourtree.SUCCESS;
+			}
+		}
+		return behaviourtree.FAIL;
+	},
+
+	SelectBuyableLot: function (context) {
+		return aiSelectBuilding(context, function(building) {
+			return (
+				building instanceof buildingVacantLot.VacantLot &&
+				building.owner === null
+			);
+		});
+	},
+
+	SelectMarketWithEdible: function (context) {
+		return aiSelectBuilding(context, function(building) {
+			return (
+				building instanceof buildingClearingHouse.ClearingHouse && (
+					building.have('pie') ||
+					building.have('jam')
+				)
+			);
+		});
+	},
+
+	SelectDynastyBuildingWithEdible: function (context) {
+		var dynastyIndex = influence.characters[context['character']].dynasty;
+		return aiSelectBuilding(context, function(building) {
+			return (
+				building.owner == dynastyIndex &&
+				typeof building.stock != 'undefined' && (
+					building.stock.containItems('jam', 1) ||
+					building.stock.containItems('pie', 1)
+				)
+			);
+		});
+	},
+
+	SelectDynastyInn: function (context) {
+		var dynastyIndex = influence.characters[context['character']].dynasty;
+		return aiSelectBuilding(context, function(building) {
+			return (
+				building instanceof buildingInn.Inn &&
+				building.owner == dynastyIndex
+			);
+		});
+	},
+
+	SelectInnWithMeal: function (context) {
+		return aiSelectBuilding(context, function(building) {
+			return (
+				building instanceof buildingInn.Inn && (
+					building.stock.containItems('pie', 1) ||
+					building.stock.containItems('jam', 1)
+				)
+			);
+		});
+	},
+
+	SelectRandomBuilding: function (context) {
+		var buildings = getBuildingsList();
+		var selected = Math.floor(Math.random() * buildings.length);
+		context['selectedBuilding'] = buildings[selected];
+		return behaviourtree.SUCCESS;
+	},
+
+	TakeEdibleFromStock: function (context) {
+		var character = influence.characters[context['character']];
+		var building = context['selectedBuilding'];
+
+		if (typeof building.stock == 'undefined') {
+			return behaviourtree.FAIL;
+		}
+
+		var edibles = ['pie', 'jam'];
+		for (var i = 0; i < edibles.length; ++i) {
+			var item = edibles[i];
+			if (character.inventory.hasSlotForItem(item) && building.stock.containItems(item, 1)) {
+				building.stock.removeItems(item, 1);
+				character.inventory.addItems(item, 1);
+				return behaviourtree.SUCCESS;
+			}
+		}
+		return behaviourtree.FAIL;
+	},
+
+	TakeMeal: function (context) {
+		var character = influence.characters[context['character']];
+		var building = context['selectedBuilding'];
+
+		var res = aiDoAction(context, {
+			action: 'meal',
+			actor: character,
+			building: building,
+			callback: aiDefaultActionCallback
+		});
+		if (res == behaviourtree.SUCCESS) {
+			context['lastEat'] = getGameDate();
+		}
+		return res;
+	}
 };
-/*eslint-enable indent*/
+
+function aiCompileNode(lines, lineNum) {
+	var node = null;
+	var line = lines[lineNum];
+	var indentLevel = line.search(/[^\t]/);
+	var nodeName = line.match(/([^\t ]+)/)[1];
+
+	if (nodeName == '->') {
+		node = {type: 'sequence'};
+	}else if (nodeName == '?') {
+		node = {type: 'selector'};
+	}else {
+		node = {
+			type: 'leaf',
+			action: aiBehaviourTreeFunctions[nodeName]
+		};
+	}
+
+	var childsLevel = indentLevel + 1;
+	var childs = [];
+	for (++lineNum; lineNum < lines.length; ++lineNum) {
+		line = lines[lineNum];
+		indentLevel = line.search(/[^\t]/);
+		if (indentLevel == -1) {
+			continue;
+		}
+		if (indentLevel > childsLevel) {
+			continue;
+		}
+		if (indentLevel < childsLevel) {
+			break;
+		}
+
+		childs.push(aiCompileNode(lines, lineNum));
+	}
+	if (childs.length > 0) {
+		node.childs = childs;
+	}
+
+	return node;
+}
+
+function aiCompileBehaviourTree(graph) {
+	var lines = graph.split('\n');
+	var root = null;
+	for (var lineNum = 0; lineNum < lines.length; ++lineNum) {
+		var line = lines[lineNum];
+		var indentLevel = line.search(/[^\t]/);
+		if (indentLevel == -1) {
+			continue;
+		}
+		root = aiCompileNode(lines, lineNum);
+		break;
+	}
+	return root;
+}
+
+var AI_CHARACTER_BEHAVIOUR = aiCompileBehaviourTree(AI_CHARACTER_BEHAVIOUR_GRAPH);
 
 function aiBehaviourVillagerTick(character) {
 	// Do nothing if already busy
@@ -152,35 +293,6 @@ function aiBehaviourVillagerTick(character) {
 
 	// Tick the behaviour tree
 	behaviourtree.tick(character.aiContext['btdata'], character.aiContext);
-}
-
-function aiBuildInn(context) {
-	return aiDoAction(context, {
-		action: 'construct',
-		buildingName: 'inn',
-		actor: influence.characters[context['character']],
-		originalLot: context['selectedBuilding'],
-		callback: aiDefaultActionCallback
-	});
-}
-
-function aiBuyOneEdible(context) {
-	var edibles = ['pie', 'jam'];
-	for (var i = 0; i < edibles.length; ++i) {
-		if (context['selectedBuilding'].buy(context['character'], 'pie', 1)) {
-			return behaviourtree.SUCCESS;
-		}
-	}
-	return behaviourtree.FAIL;
-}
-
-function aiBuyVacantLot(context) {
-	return aiDoAction(context, {
-		action: 'buy',
-		actor: influence.characters[context['character']],
-		target: context['selectedBuilding'],
-		callback: aiDefaultActionCallback
-	});
 }
 
 function aiDefaultActionCallback(characterIndex, actionOrder, res) {
@@ -237,62 +349,6 @@ function aiDoAction(context, actionOrder, successCheck) {
 	return behaviourtree.RUNNING;
 }
 
-function aiGoToBuilding(context) {
-	var character = influence.characters[context['character']];
-	if (character.currentAction == 'move') {
-		return behaviourtree.RUNNING;
-	}
-	if (context['selectedBuilding'] === null) {
-		return behaviourtree.FAIL;
-	}
-	var building = context['selectedBuilding'];
-	if (character.x == building.x && character.y == building.y) {
-		return behaviourtree.SUCCESS;
-	}
-	moveCharacter(character, {x:building.x, y:building.y}, building.indoor);
-	return behaviourtree.RUNNING;
-}
-
-function aiHaveEdibleInInventory(context) {
-	var character = influence.characters[context['character']];
-	if (
-		character.inventory.containItems('pie', 1) ||
-		character.inventory.containItems('jam', 1)
-	)
-	{
-		return behaviourtree.SUCCESS;
-	}
-	return behaviourtree.FAIL;
-}
-
-function aiIsHungry(context) {
-	var hoursSinceLastMeal = (getGameDate().getTime() - context['lastEat'].getTime()) / (60*60*1000);
-	if (hoursSinceLastMeal >= 24) {
-		return behaviourtree.SUCCESS;
-	}
-	return behaviourtree.FAIL;
-}
-
-function aiPutEdibleToStock(context) {
-	var character = influence.characters[context['character']];
-	var building = context['selectedBuilding'];
-
-	if (typeof building.stock == 'undefined') {
-		return behaviourtree.FAIL;
-	}
-
-	var edibles = ['pie', 'jam'];
-	for (var i = 0; i < edibles.length; ++i) {
-		var item = edibles[i];
-		if (building.stock.hasSlotForItem(item) && character.inventory.containItems(item, 1)) {
-			character.inventory.removeItems(item, 1);
-			building.stock.addItems(item, 1);
-			return behaviourtree.SUCCESS;
-		}
-	}
-	return behaviourtree.FAIL;
-}
-
 function aiSelectBuilding(context, filter) {
 	var buildings = getBuildingsList();
 	var eligibleBuildings = [];
@@ -308,101 +364,4 @@ function aiSelectBuilding(context, filter) {
 	var selectedBuilding = Math.floor(Math.random() * eligibleBuildings.length);
 	context['selectedBuilding'] = eligibleBuildings[selectedBuilding];
 	return behaviourtree.SUCCESS;
-}
-
-function aiSelectBuyableVacantLot(context) {
-	return aiSelectBuilding(context, function(building) {
-		return (
-			building instanceof buildingVacantLot.VacantLot &&
-			building.owner === null
-		);
-	});
-}
-
-function aiSelectClearingHouseHavingEdible(context) {
-	return aiSelectBuilding(context, function(building) {
-		return (
-			building instanceof buildingClearingHouse.ClearingHouse && (
-				building.have('pie') ||
-				building.have('jam')
-			)
-		);
-	});
-}
-
-function aiSelectDynastyBuildingStockingEdible(context) {
-	var dynastyIndex = influence.characters[context['character']].dynasty;
-	return aiSelectBuilding(context, function(building) {
-		return (
-			building.owner == dynastyIndex &&
-			typeof building.stock != 'undefined' && (
-				building.stock.containItems('jam', 1) ||
-				building.stock.containItems('pie', 1)
-			)
-		);
-	});
-}
-
-function aiSelectDynastyInn(context) {
-	var dynastyIndex = influence.characters[context['character']].dynasty;
-	return aiSelectBuilding(context, function(building) {
-		return (
-			building instanceof buildingInn.Inn &&
-			building.owner == dynastyIndex
-		);
-	});
-}
-
-function aiSelectInnWithMeal(context) {
-	return aiSelectBuilding(context, function(building) {
-		return (
-			building instanceof buildingInn.Inn && (
-				building.stock.containItems('pie', 1) ||
-				building.stock.containItems('jam', 1)
-			)
-		);
-	});
-}
-
-function aiSelectRandomBuilding(context) {
-	var buildings = getBuildingsList();
-	var selected = Math.floor(Math.random() * buildings.length);
-	context['selectedBuilding'] = buildings[selected];
-	return behaviourtree.SUCCESS;
-}
-
-function aiTakeEdibleFromStock(context) {
-	var character = influence.characters[context['character']];
-	var building = context['selectedBuilding'];
-
-	if (typeof building.stock == 'undefined') {
-		return behaviourtree.FAIL;
-	}
-
-	var edibles = ['pie', 'jam'];
-	for (var i = 0; i < edibles.length; ++i) {
-		var item = edibles[i];
-		if (character.inventory.hasSlotForItem(item) && building.stock.containItems(item, 1)) {
-			building.stock.removeItems(item, 1);
-			character.inventory.addItems(item, 1);
-			return behaviourtree.SUCCESS;
-		}
-	}
-	return behaviourtree.FAIL;
-}
-
-function aiTakeMeal(context) {
-	var character = influence.characters[context['character']];
-	var building = context['selectedBuilding'];
-
-	var res = aiDoAction(context, {
-		action: 'meal',
-		actor: character,
-		building: building,
-		callback: aiDefaultActionCallback
-	});
-	if (res == behaviourtree.SUCCESS) {
-		context['lastEat'] = getGameDate();
-	}
-	return res;
 }
